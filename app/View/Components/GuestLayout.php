4786bd275b51c22b3f00d577d101560728c73288
DulangaMendis <dulangamendis82@gmail.com>
2025-12-06 21:29:01 +0530

    set up login rout

--- app/View/Components/GuestLayout.php
+++ app/View/Components/GuestLayout.php
@@ -10,7 +10,7 @@ class GuestLayout extends Component
     /**
      * Get the view / contents that represents the component.
      */
-    public function render(): View
+    public function render(): View|Closure|string
     {
         return view('layouts.guest');
     }
--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,8 @@ Route::get('/', function () {
     return view('frontend.Home');
 });
 
-Route::get('/testadmin', function () {
-    return view('admin.dashboard');
-});
-
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
